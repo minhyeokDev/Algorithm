a0cc8cdcb94bfb905f53e24eae0127761380a98e
Minhyeok Song <125117389+minhyeokDev@users.noreply.github.com>
2024-01-24 18:12:46 +0900

    [Silver V] Title: 색종이, Time: 128 ms, Memory: 14140 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine()); //색종이 수
+
+        int total = 0; // 검은 영역 넓이
+        boolean[][] paper = new boolean[100][100];
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            for (int j = x; j < x + 10; j++) {
+                for (int k = y; k < y + 10; k++) {
+                    if (!paper[j][k]) {
+                        paper[j][k] = true;
+                        total++;
+                    }
+                }
+            }
+
+        }
+        System.out.println(total);
+
+
+    }
+}

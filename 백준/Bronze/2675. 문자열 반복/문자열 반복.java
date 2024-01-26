016c220ca6732410aae4069715561b2998fff1a8
Minhyeok Song <125117389+minhyeokDev@users.noreply.github.com>
2024-01-26 19:14:14 +0900

    [Bronze II] Title: 문자열 반복, Time: 120 ms, Memory: 14256 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.java
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.java
@@ -12,17 +12,15 @@ public class Main {
         int test = Integer.parseInt(br.readLine());
 
         StringBuilder sb;
-        String test1;
-        String test2;
 
         String[] strings = new String[test];
         for (int i = 0; i < test; i++) {
             sb = new StringBuilder();
             st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
+            int R = Integer.parseInt(st.nextToken());
             String s = st.nextToken();
             for (int j = 0; j < s.length(); j++) {
-                for (int k = 0; k < a; k++) {
+                for (int k = 0; k < R; k++) {
                     sb.append(s.charAt(j));
                 }
             }

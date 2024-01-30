72a4fafe445789f3cc768c1b554df0ae2a9224cc
Minhyeok Song <125117389+minhyeokDev@users.noreply.github.com>
2024-01-31 01:40:56 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1824 ms, Memory: 300976 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -1,21 +1,32 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
+        int[] cnt = new int[10001];
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
+        //int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
+            cnt[Integer.parseInt(br.readLine())]++;
+            //arr[i] = Integer.parseInt(br.readLine());
         }
-        Arrays.sort(arr);
+
+        br.close();
+
+        for (int i = 1; i < 10001; i++) {
+            while (cnt[i] > 0) {
+                bw.write(i + "\n");
+                cnt[i]--;
+            }
+        }
+/*        Arrays.sort(arr);
         for (int i = 0; i < n; i++) {
             bw.write(arr[i] + "\n");
-        }
+        }*/
 
         bw.flush();
 

be740413db60f714fd6264f5a4cb6f12b8ecfee0
Minhyeok Song <125117389+minhyeokDev@users.noreply.github.com>
2024-02-28 17:36:20 +0900

    [Silver V] Title: 방 번호, Time: 128 ms, Memory: 14312 KB -BaekjoonHub

---
+++ 백준/Silver/1475. 방 번호/방 번호.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s = br.readLine();
+		int N = Integer.parseInt(s);
+		int[] arr = new int[10];
+
+		for (int i = 0; i < s.length(); i++) {
+			int num = Character.getNumericValue(s.charAt(i));
+			if (num == 6) {
+				arr[9] ++;
+			}
+			else {
+				arr[num]++;
+			}
+		}
+		int max = 0;
+		for (int i = 0; i < 9; i++) {
+			max = Math.max(arr[i], max);
+		}
+		int nine = arr[9] / 2 + (arr[9] % 2);
+
+		max = Math.max(max, nine);
+
+		System.out.println(max);
+
+	}
+}

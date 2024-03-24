4b0d3ed153ee59e6bfc105ae732f0ed0e1693a8a
Minhyeok Song <125117389+minhyeokDev@users.noreply.github.com>
2024-03-24 23:05:30 +0900

    [Silver V] Title: 줄 세우기, Time: 128 ms, Memory: 14200 KB -BaekjoonHub

---
+++ 백준/Silver/11536. 줄 세우기/줄 세우기.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n,i,s;
+		n = Integer.parseInt(br.readLine());
+		String[] name = new String[n];
+		for(i=0;i<n;i++) name[i] = br.readLine();
+		s = 0;
+		for(i=0;i<n-1;i++) s += name[i].compareTo(name[i+1])>0?1:-1;
+		if(s==n-1) {
+			System.out.println("DECREASING");
+		}else if(s==1-n) {
+			System.out.println("INCREASING");
+		}else {
+			System.out.println("NEITHER");
+		}
+	}
+}

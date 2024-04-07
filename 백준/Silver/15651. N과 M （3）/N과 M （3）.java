df74019111a132550a195906f9928336dcfeeccb
Minhyeok Song <125117389+minhyeokDev@users.noreply.github.com>
2024-04-07 23:45:43 +0900

    [Silver III] Title: N과 M (3), Time: 456 ms, Memory: 69212 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static int[] arr;
+    public static StringBuilder sb = new StringBuilder();
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        arr = new int[m];
+        dfs(n, m, 0);
+        System.out.println(sb);
+    }
+
+
+    public static void dfs(int N, int M, int depth) {
+        if (depth == M) {
+            for (int val : arr) {
+                sb.append(val).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+                arr[depth] = i + 1;
+                dfs(N, M, depth + 1);
+        }
+    }
+}

685f249b67db00985f553255319e03132207c7ae
Minhyeok Song <125117389+minhyeokDev@users.noreply.github.com>
2024-05-12 19:31:33 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 128 ms, Memory: 14112 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	static int[] d = new int[20];
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		d[1] = 1;
+		d[2] = 2;
+		d[3] = 4;
+		for(int i = 4 ; i < 11 ; i++) {
+			d[i] = d[i-1] + d[i-2] + d[i-3];
+		}
+		int t = Integer.parseInt(br.readLine());
+
+		while(t-- > 0) {
+			int n = Integer.parseInt(br.readLine());
+			System.out.println(d[n]);
+		}
+
+	}
+
+}

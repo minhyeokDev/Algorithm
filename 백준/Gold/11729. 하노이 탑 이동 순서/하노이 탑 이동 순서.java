1c1e1f18e5ef918f7679efaa726cc95f21450755
Minhyeok Song <125117389+minhyeokDev@users.noreply.github.com>
2024-07-01 11:13:12 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 524 ms, Memory: 77296 KB -BaekjoonHub

--- 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
@@ -7,27 +7,23 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		int k = Integer.parseInt(br.readLine());
-		sb.append((int) Math.pow(2,k) -1).append("\n");
-		hanoi(1, 3, k);
-		System.out.println(sb);
 
+		int n = Integer.parseInt(br.readLine());
+		sb.append((int) Math.pow(2, n) - 1).append("\n");
+		func(1, 3, n);
+		System.out.println(sb);
 		br.close();
 	}
 
-	public static void hanoi(int a, int b, int n) {
-
-		if (n == 1) { //base conditon
+	public static void func(int a, int b, int n) {
+		if (n == 1) {
 			sb.append(a + " " + b).append("\n");
 			return;
 		}
-		hanoi(a, 6 - a - b, n - 1);
+		func(a, 6 - a - b, n - 1);
 		sb.append(a + " " + b).append("\n");
-		hanoi(6 - a - b, b, n - 1);
-
+		func(6 - a - b, b, n - 1);
 
 	}
-
 
 }

b929e7ee3682c0cce07ff7e5095d47b87f4ba409
Minhyeok Song <125117389+minhyeokDev@users.noreply.github.com>
2024-07-21 17:31:30 +0900

    [Silver IV] Title: 카드, Time: 380 ms, Memory: 38912 KB -BaekjoonHub

--- 백준/Silver/11652. 카드/카드.java
+++ 백준/Silver/11652. 카드/카드.java
@@ -3,29 +3,30 @@ import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) throws NumberFormatException, IOException {
+	public static void main(String[] arsgs) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+		int n = Integer.parseInt(br.readLine());
 
-        HashMap<Long, Integer> cards = new HashMap<>();
+		HashMap<Long, Integer> cards = new HashMap<>();
+		int max =0;
+		long x = 0;
+		for (int i = 0; i < n; i++) {
+			long card = Long.parseLong(br.readLine());
+			cards.put(card, cards.getOrDefault(card, 0) + 1);
 
-        int max = 0;
-        long x = 0;
-        for (int i = 0; i < N; i++) {
-            long card = Long.parseLong(br.readLine());
+			if (cards.get(card) > max) {
+				max = cards.get(card);
+				x = card;
+			} else if (cards.get(card) == max) {
+				x = Math.min(x, card);
+			}
 
-            cards.put(card, cards.getOrDefault(card, 0) + 1);
+		}
 
-            if (cards.get(card) > max) {
-                max = cards.get(card);
-                x = card;
-            } else if (cards.get(card) == max) {
-                x = Math.min(x, card);
-            }
-        }
+		System.out.println(x);
 
-        System.out.println(x);
+		br.close();
+	}
 
-    }
 }

52550a3f6ae5d9c0905031b9b799439b5100755f
Minhyeok Song <125117389+minhyeokDev@users.noreply.github.com>
2024-07-27 23:24:12 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 124 ms, Memory: 14160 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -1,25 +1,29 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
 
-	static int[] d = new int[20];
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int[] d = new int[20];
 
-		d[1] = 1;
-		d[2] = 2;
-		d[3] = 4;
-		for(int i = 4 ; i < 11 ; i++) {
-			d[i] = d[i-1] + d[i-2] + d[i-3];
-		}
-		int t = Integer.parseInt(br.readLine());
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		while(t-- > 0) {
-			int n = Integer.parseInt(br.readLine());
-			System.out.println(d[n]);
-		}
+        d[1] = 1;
+        d[2] = 2;
+        d[3] = 4;
+        for (int i = 4; i < 11; i++) {
+            d[i] = d[i - 1] + d[i - 2] + d[i - 3];
+        }
 
-	}
+        int t = Integer.parseInt(br.readLine());
+
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine());
+            System.out.println(d[n]);
+        }
+
+        br.close();
+    }
 
 }

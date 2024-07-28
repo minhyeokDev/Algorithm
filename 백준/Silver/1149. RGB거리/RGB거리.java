365ad7be5ddd35ea12ad1ae71d20bea3c8b913bc
Minhyeok Song <125117389+minhyeokDev@users.noreply.github.com>
2024-07-28 10:58:35 +0900

    [Silver I] Title: RGB거리, Time: 140 ms, Memory: 14660 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -3,32 +3,32 @@ import java.util.*;
 
 public class Main {
 
-	static int[][] d = new int[1005][3];
-	static int[] r = new int[1005];
-	static int[] g = new int[1005];
-	static int[] b = new int[1005];
+    static int[][] d = new int[1005][3];
+    static int[] r = new int[1005];
+    static int[] g = new int[1005];
+    static int[] b = new int[1005];
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		int n = Integer.parseInt(br.readLine());
-		for(int i = 1; i <= n; i++){
-			st = new StringTokenizer(br.readLine());
-			r[i] = Integer.parseInt(st.nextToken());
-			g[i] = Integer.parseInt(st.nextToken());
-			b[i] = Integer.parseInt(st.nextToken());
-		}
-		d[1][0] = r[1];
-		d[1][1] = g[1];
-		d[1][2] = b[1];
-		for(int i = 2; i <= n; i++){
-			d[i][0] = Math.min(d[i-1][1],d[i-1][2])+r[i];
-			d[i][1] = Math.min(d[i-1][0],d[i-1][2])+g[i];
-			d[i][2] = Math.min(d[i-1][0],d[i-1][1])+b[i];
-		}
-		System.out.println(Math.min(Math.min(d[n][0], d[n][1]), d[n][2]));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine());
+            r[i] = Integer.parseInt(st.nextToken());
+            g[i] = Integer.parseInt(st.nextToken());
+            b[i] = Integer.parseInt(st.nextToken());
+        }
+        d[1][0] = r[1];
+        d[1][1] = g[1];
+        d[1][2] = b[1];
+        for (int i = 2; i <= n; i++) {
+            d[i][0] = Math.min(d[i - 1][1], d[i - 1][2]) + r[i];
+            d[i][1] = Math.min(d[i - 1][0], d[i - 1][2]) + g[i];
+            d[i][2] = Math.min(d[i - 1][0], d[i - 1][1]) + b[i];
+        }
+        System.out.println(Math.min(Math.min(d[n][0], d[n][1]), d[n][2]));
 
-		br.close();
-	}
+        br.close();
+    }
 
 }

a30493fc9bf56d3d5204e1cccdd177486115c01f
Minhyeok Song <125117389+minhyeokDev@users.noreply.github.com>
2024-07-30 16:25:25 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 1500 ms, Memory: 58992 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,35 +1,39 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class Main {
 
-	static int n, m;
-	static int[] a = new int[100004];
-	static int[] d = new int[100004];
+    static int n, m;
+    static int[] a = new int[100004];
+    static int[] d = new int[100004];
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
 
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-		d[0] = 0;
-		st = new StringTokenizer(br.readLine());
-		for (int i = 1; i <= n; i++) {
-			a[i] = Integer.parseInt(st.nextToken());
-			d[i] = d[i - 1] + a[i];
-		}
-		while (m-- > 0) {
-			int i, j;
-			st = new StringTokenizer(br.readLine());
-			i = Integer.parseInt(st.nextToken());
-			j = Integer.parseInt(st.nextToken());
-			System.out.println(d[j] - d[i - 1]);
-		}
+        // D[i] = A[1] + A[2] + ... + A[i]
+        // D[i] = D[i-1] + A[i]
 
-	}
+        d[0] = 0;
+        
+        for (int i = 1; i <= n; i++) {
+            a[i] = Integer.parseInt(st.nextToken());
+            d[i] = d[i - 1] + a[i];
+        }
+        
+        while (m-- > 0) {
+            int i, j;
+            st = new StringTokenizer(br.readLine());
+            i = Integer.parseInt(st.nextToken());
+            j = Integer.parseInt(st.nextToken());
+            int sum = d[j] - d[i - 1];
+            System.out.println(sum);
+        }
 
+        br.close();
 
+    }
 }

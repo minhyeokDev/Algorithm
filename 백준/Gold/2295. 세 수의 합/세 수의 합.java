e064f4fb81fd8cb852b1a938340eb9b51ffa217a
Minhyeok Song <125117389+minhyeokDev@users.noreply.github.com>
2024-09-05 14:00:16 +0900

    [Gold IV] Title: 세 수의 합, Time: 312 ms, Memory: 20580 KB -BaekjoonHub

--- 백준/Gold/2295. 세 수의 합/세 수의 합.java
+++ 백준/Gold/2295. 세 수의 합/세 수의 합.java
@@ -1,34 +1,67 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Scanner;
-
+import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int[] arr = new int[N];
-        for (int i = 0; i < N; i++)arr[i] = sc.nextInt();
-        List<Integer> sum = new ArrayList<>();
-        for (int i = 0 ; i < N ; i++){
-            for (int j = 0 ; j < N; j++){
-                sum.add(arr[i] + arr[j]);
+
+    static int n;
+    static int[] a;
+    static int[] two;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+
+        a = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            a[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(a);
+
+        two = new int[(a.length * (a.length + 1)) / 2];
+        int count = 0;
+        for (int i = 0; i < a.length; i++) {
+            for (int j = i; j < a.length; j++) { // 중복 최소화 하기 위해 j=i부터
+                two[count++] = a[i] + a[j];
             }
         }
-        Arrays.sort(arr);
-        Collections.sort(sum);
 
-        for (int i = N-1; i>=0; i--){
-            for (int j = N-1; j>=0; j--){
-                int minus = arr[i] - arr[j];
+        Arrays.sort(two);
+        int answer = -1;
 
-                if (Collections.binarySearch(sum,minus)>=0){
-                    System.out.println(arr[i]);
-                    return;
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a.length; j++) {
+                int target = a[i] - a[j];
+//                //binarySearch 직접 구현 방식
+//                if (binarySearch(a[i] - a[j])) {
+//                    answer = Math.max(answer, a[i]);
+//                }
+                if (Arrays.binarySearch(two, target) > -1) {
+                    answer = Math.max(answer, a[i]);
                 }
             }
         }
+
+        System.out.println(answer);
+
+        br.close();
     }
+
+//    public static boolean binarySearch(int target) {
+//        int st = 0;
+//        int en = two.length - 1;
+//        while (st <= en) {
+//            int mid = (st + en) / 2;
+//            if (two[mid] < target) {
+//                st = mid + 1;
+//            } else if (two[mid] > target) {
+//                en = mid - 1;
+//            } else {
+//                return true;
+//            }
+//        }
+//        return false;
+//    }
+
 }

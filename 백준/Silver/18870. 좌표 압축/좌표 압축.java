b089ce125ee0e1b5c1ca143cee1778ff165c39ae
Minhyeok Song <125117389+minhyeokDev@users.noreply.github.com>
2024-09-05 12:46:41 +0900

    [Silver II] Title: 좌표 압축, Time: 1708 ms, Memory: 238040 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.java
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -1,42 +1,45 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
+
+    static int n;
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
 
-        int N = Integer.parseInt(br.readLine());
-
-        int[] origin = new int[N];
-        int[] sorted = new int[N];
-        HashMap<Integer, Integer> map = new HashMap<>();
-
+        int[] origin = new int[n];
+        int[] sorted = new int[n];
+        HashMap<Integer, Integer> rankingMap = new HashMap<>();
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < N; i++) {
+        for (int i = 0; i < n; i++) {
             sorted[i] = origin[i] = Integer.parseInt(st.nextToken());
         }
 
         Arrays.sort(sorted);
 
         int rank = 0;
-        for(int v : sorted) {
-            if(!map.containsKey(v)) {
-                map.put(v, rank);
+
+        for (int v : sorted) {
+            if (!rankingMap.containsKey(v)) {
+                rankingMap.put(v, rank);
                 rank++;
             }
         }
 
         StringBuilder sb = new StringBuilder();
-        for(int key : origin) {
-            int ranking = map.get(key);
+
+        for (int key : origin) {
+            int ranking= rankingMap.get(key);
             sb.append(ranking).append(' ');
         }
 
-        System.out.println(sb);
+        System.out.print(sb);
 
+        br.close();
 
     }
 }

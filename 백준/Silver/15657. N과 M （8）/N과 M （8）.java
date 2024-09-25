823ce585b975400e08fd9993e2026c8bd42e67cf
Minhyeok Song <125117389+minhyeokDev@users.noreply.github.com>
2024-09-25 17:16:50 +0900

    [Silver III] Title: N과 M (8), Time: 176 ms, Memory: 20788 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int n, m;
+    static int[] arr = new int[10];
+    static int[] num;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        num = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            num[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(num);
+        func(0, 0);
+        System.out.println(sb);
+
+        br.close();
+
+    }
+
+    public static void func(int k, int start) {
+        if (k == m) {
+            for (int i = 0; i < m; i++) {
+                sb.append(num[arr[i]] + " ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for (int i = start; i < n; i++) {
+            arr[k] = i;
+            func(k + 1, i);
+        }
+
+    }
+
+}

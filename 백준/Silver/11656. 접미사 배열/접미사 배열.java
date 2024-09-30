ddc6a17d38f82536655b85f01a6c717853ace938
Minhyeok Song <125117389+minhyeokDev@users.noreply.github.com>
2024-09-30 21:42:28 +0900

    [Silver IV] Title: 접미사 배열, Time: 136 ms, Memory: 17724 KB -BaekjoonHub

--- 백준/Silver/11656. 접미사 배열/접미사 배열.java
+++ 백준/Silver/11656. 접미사 배열/접미사 배열.java
@@ -10,17 +10,17 @@ public class Main {
         String s = br.readLine();
         String[] arr = new String[s.length()];
         for (int i = 0; i < s.length(); i++) {
-            arr[i] = "";
-            for (int j = i; j < s.length(); j++) {
-                arr[i] += s.charAt(j);
-            }
+            //arr[i] = "";
+//            for (int j = i; j < s.length(); j++) {
+//                arr[i] += s.charAt(j);
+//            }
+            arr[i] = s.substring(i, s.length());
         }
 
         Arrays.sort(arr);
         for (int i = 0; i < s.length(); i++) {
             sb.append(arr[i]).append("\n");
         }
-
 
         bw.write(String.valueOf(sb));
         bw.flush();

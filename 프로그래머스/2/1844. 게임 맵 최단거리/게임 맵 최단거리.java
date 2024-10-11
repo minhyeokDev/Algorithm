e28768f2943785fdb536760ed0df85320b9f063f
Minhyeok Song <125117389+minhyeokDev@users.noreply.github.com>
2024-10-11 23:23:50 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 11.83 ms, Memory: 54.6 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -4,19 +4,19 @@ class Solution {
     static int[] dx = {-1,0,1,0};
     static int[] dy = {0,1,0,-1};
     static Queue<Pair> q = new LinkedList<>();
-    static int[][] visited;
+    static boolean[][] visited;
     public int solution(int[][] maps) {
         int n = maps.length;
         int m = maps[0].length;
         // 캐릭터 위치 큐에 추가
         q.offer(new Pair(0,0));
-        visited = new int[n][m];
+        visited = new boolean[n][m];
         
         int temp = bfs(maps, visited, n, m);
         return temp;
     }
-    public static int bfs(int[][] maps, int[][] visited, int n, int m) {
-        visited[0][0] = 1;
+    public static int bfs(int[][] maps, boolean[][] visited, int n, int m) {
+        visited[0][0] = true;
         while(!q.isEmpty()) {
             Pair p = q.poll();
             
@@ -27,17 +27,18 @@ class Solution {
                 if( nX < 0 || nX >= n || nY <0 || nY >= m) {
                     continue;
                 }
-                if(visited[nX][nY] == 0&& maps[nX][nY] != 0) {
-                    visited[nX][nY] = visited[p.x][p.y] + 1 ;
+                if(!visited[nX][nY] && maps[nX][nY] != 0) {
+                    visited[nX][nY] = true;
+                    maps[nX][nY] = maps[p.x][p.y] + 1;
                     q.offer(new Pair(nX,nY));
                 }
             }
         }
         
-        if(visited[n-1][m-1] == 0) {
+        if(visited[n-1][m-1] == false) {
             return -1;
         }
-        return visited[n-1][m-1];
+        return maps[n-1][m-1];
         
     }
     

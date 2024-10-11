fb612a838ab4b06388347e7ee7ccdb4cbb9f7cf7
Minhyeok Song <125117389+minhyeokDev@users.noreply.github.com>
2024-10-11 14:12:35 +0900

    [level 2] Title: 소수 찾기, Time: 24.69 ms, Memory: 81.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String numbers) {
+        StringBuilder sb = new StringBuilder();
+        String newNumber;
+        int answer = 0;
+        for(int i = 0 ; i < numbers.length();i++) {
+            
+        }
+        return answer;
+    }
+}

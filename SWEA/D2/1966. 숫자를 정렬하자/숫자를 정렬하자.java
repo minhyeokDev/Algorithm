cc3ca05b8eedbb74a6ab06af85f28be09700324c
Minhyeok Song <125117389+minhyeokDev@users.noreply.github.com>
2024-11-12 13:37:54 +0900

    [D2] Title: 숫자를 정렬하자, Time: 116 ms, Memory: 20,252 KB -BaekjoonHub

---
+++ SWEA/D2/1966. 숫자를 정렬하자/숫자를 정렬하자.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int T = Integer.parseInt(br.readLine());
+
+        for (int t = 1; t <= T; t++) {
+            int n = Integer.parseInt(br.readLine());
+            int[] arr = new int[n];
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(st.nextToken());
+            }
+            Arrays.sort(arr);
+
+            System.out.print("#" + t + " ");
+            for (int i = 0; i < arr.length; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+
+        }
+
+        br.close();
+    }
+
+}
